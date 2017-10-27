c4f63d1e6fbae411e5d78c908584b2f3c43795a5
ANGELA EBIRIM <ebirimac@gmail.com>
2017-10-27 21:43:47 +0100

    addition of test for document model

--- spec/models/hybrid_bill_document_spec.rb
+++ spec/models/hybrid_bill_document_spec.rb
@@ -12,9 +12,14 @@ RSpec.describe HybridBillDocument do
 
 	context 'decoding' do   
 
-	  it 'is ok' do
+	  it 'is working' do
 	  	hybrid_bill_document = build(:hybrid_bill_document)
 	    expect(Base64.decode64(hybrid_bill_document.document_data)).to eq("This is a test string")
+	  end
+
+	   it 'is not working' do
+	  	hybrid_bill_document = build(:hybrid_bill_document, document_data: "This is a test string")
+	    expect(Base64.decode64(hybrid_bill_document.document_data)).not_to eq("This is a test string")
 	  end
 
 	end  
