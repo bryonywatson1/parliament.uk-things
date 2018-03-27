f14d55c693aa2600317e34950dc22635a35ad75b
Bryony Watson <watsonb@parliament.uk>
2018-03-27 16:06:55 +0100

    started refactoring but paused

--- app/controllers/concerns/person_description_helper.rb
+++ app/controllers/concerns/person_description_helper.rb
@@ -1,11 +1,28 @@
-%p.lead
-  - sentence_array = ["#{@person.display_name}"]
+module PersonDescriptionHelper
+
+  # Returns year_separator variable (getter method)
+  #
+  # @return [Integer] Number of years used to divide up the timeline into time periods
+  def self.year_separator
+    @year_separator ||= 10
+  end
+
+  def self.sentence_array
+    @sentence_array ||= ["#{@person.display_name}"]
+  end
+
+  def self.create_sentence
+    current_roles.each do |role|
+      if role == current_roles.first
+        @sentence_array.push(" is currently #{role.government_position.name}"
+  end
+
+
   - current_roles.fetch('GovernmentIncumbency', []).each do |role|
     - sentence_array.push(" is currently #{role.government_position.name}")
 
   - opposition_roles = current_roles.fetch('OppositionIncumbency', [])
   - opposition_roles.each do |role|
-    - p role.type
     - if current_roles.include?('GovernmentIncumbency')
       - sentence_array.push(", #{role.opposition_position.name}")
     - else
@@ -36,3 +53,6 @@
     - else
       - sentence_array.push(" became a Member of the House of Lords in #{l(@sorted_incumbencies.first.start_date, format: :year_only)}.")
   = sentence_array.join
+
+
+end
