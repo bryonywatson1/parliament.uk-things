da1656960c02d992fe243dcf2eb59db7a28cd34e
Bryony Watson <watsonb@parliament.uk>
2018-04-09 15:45:01 +0100

    description helper

--- app/controllers/concerns/person_description_helper.rb
+++ app/controllers/concerns/person_description_helper.rb
@@ -12,9 +12,30 @@ module PersonDescriptionHelper
   end
 
   def self.create_sentence
+    current_roles.sort_by!(:type)
     current_roles.each do |role|
-      if role == current_roles.first
-        @sentence_array.push(" is currently #{role.government_position.name}"
+      unless role.type == 'SeatIncumbency'
+        if role == current_roles.first
+          @sentence_array.push(" is currently #{role.government_position.name}") if role.type == 'GovernmentIncumbency'
+          @sentence_array.push(" is currently #{role.opposition_position.name}") if role.type == 'OppositionIncumbency'
+          @sentence_array.push(" is currently a #{t('member').capitalize} #{t('of_the')} #{role.formal_body.name}") if role.type == 'FormalBodyMembership'
+        elsif role == current_roles.last
+          sentence_array.push(" and #{role.opposition_position.name}") if role.type == 'OppositionIncumbency'
+          sentence_array.push(" and the #{role.formal_body.name}") if role.type == 'FormalBodyMembership'
+        else
+          @sentence_array.push(", #{role.government_position.name}") if role.type == 'GovernmentIncumbency'
+          @sentence_array.push(", #{role.opposition_position.name}") if role.type == 'OppositionIncumbency'
+          @sentence_array.push(", #{role.formal_body.name}") if role.type == 'FormalBodyMembership'
+        end
+      end
+      if role.type == 'SeatIncumbency'
+        if current_roles.include?('GovernmentIncumbency') || current_roles.include?('OppositionIncumbency') || current_roles.include?('FormalBodyMembership')
+          @sentence_array.push(". #{@person.pronoun}")
+        if role.class == GroupingHelper::GroupedObject || role&.house_of_commons?
+          @sentence_array.push(" became an MP in #{l(@sorted_incumbencies.first.start_date, format: :year_only)}.")
+        else
+          @sentence_array.push(" became a Member of the House of Lords in #{l(@sorted_incumbencies.first.start_date, format: :year_only)}.")
+    end
   end
 
 
